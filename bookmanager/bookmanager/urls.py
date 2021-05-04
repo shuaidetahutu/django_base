ed2a76b8f385b28d7f3020659b28cfd36b6ccea5
wwl <258003407@qq.com>
2021-05-04 20:35:23 +0800

    子应用的url

--- bookmanager/bookmanager/urls.py
+++ bookmanager/bookmanager/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+# from book.views import index
 
-from book.views import index
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('index/', index),
+    # path('index/', index),
+    # path('blog/', include('book.urls')),
+    path('', include('book.urls')),
 ]
