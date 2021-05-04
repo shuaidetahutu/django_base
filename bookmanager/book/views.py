73fd8385e4e0c93434842572bad445fdae4330bc
wwl <258003407@qq.com>
2021-05-04 19:03:06 +0800

    路由匹配

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpRequest
+from django.http import HttpResponse
+
+
+# hope consumer input 172.0.0.1:8000/index/
+# to visit views
+def index(request):
+    return HttpResponse('OK!')
