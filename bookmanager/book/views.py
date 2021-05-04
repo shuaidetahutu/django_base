44386b36b4fe462839ce2611a725e93ea08026af
wwl <258003407@qq.com>
2021-05-04 22:05:21 +0800

    模板

--- bookmanager/book/views.py
+++ bookmanager/book/views.py
@@ -9,4 +9,11 @@ from django.http import HttpResponse
 # hope consumer input 172.0.0.1:8000/index/
 # to visit views
 def index(request):
-    return HttpResponse('OK!')
+    # return HttpResponse('OK!')
+
+    # 模拟数据查询
+    context = {
+        'name': 'Double eleven now，Click to have surprise!'
+    }
+
+    return render(request, 'book/index.html', context=context)
